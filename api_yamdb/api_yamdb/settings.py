14db16efe90e21045ff68819d4b741b8aac2ea22
Илья Алферов <iliya.alferov2015@yandex.ru>
2022-08-22 23:31:58 +0300

    Debug1

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -6,7 +6,6 @@ from datetime import timedelta
 
 load_dotenv()
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
