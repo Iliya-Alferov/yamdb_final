9df88376a1e9927a9ec1982c5bfb3d726f78ae2b
Илья Алферов <iliya.alferov2015@yandex.ru>
2022-08-24 18:02:34 +0300

    test3

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -17,7 +17,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1',
                  'localhost',
-                 '84.201.137.225'
+                 '84.201.137.225',
                  'web',
                  ]
 
